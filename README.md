4fca95960f938fc1155d89c43fb017f7dd1eea10
unknown <28370491+BeatsBassBoom@users.noreply.github.com>
2018-12-11 21:52:43 -0500

    ex48 in progress new logo added to readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# LP3THW
+# LP3THW (Learn Python 3 The Hard Way) with BeatsBassBoom
+
+<img src="" align="right"
+     title="Logo of BeatsBassBoom" width="120" height="178">
+
+This repository is just all my code from LP3THW. In order to hold myself accountable for frequent coding I started this repository. It also served as practice to learn Git, Markdown, and all the comes with coding Cybersecurity tools.
+
+## Getting Started
+
+There are no instructions really. This is not a tool for anyone to run, just all my coding exercises. If you wish to learn Python in the manner I did I request you pick up the LP3THW text and go through it all everyday like I did. It really was hard but I mastered much of the language over the course of ~45 days.
+
+## Authors
+
+- **BeatsBassBoom** - _Just Starting Out In Coding_ [My Repo](https://github.com/BeatsBassBoom)
+
+## License
+
+This project has no licenses just my own original ideas on top of the work in LP3THW.
---
+++ projects/ex48-AdvancedUserInput/tests/advanced_user_input_tests.py
@@ -0,0 +1,49 @@
+from nose.tools import *
+from ex48 import advanced_user_input as aui
+
+
+def test_directions():
+    assert_equal(aui.scan("north"), [('direction', 'north')])
+    result = aui.scan("north south east")
+    assert_equal(result, [('direction', 'north'),
+                          ('direction', 'south'),
+                          ('direction', 'east')])
+
+
+def test_verbs():
+    assert_equal(aui.scan("go"), [('verb', 'go')])
+    result = aui.scan("go kill eat")
+    assert_equal(result, [('verb', 'go'),
+                          ('verb', 'kill'),
+                          ('verb', 'eat')])
+
+
+def test_stops():
+    assert_equal(aui.scan("the"), [('stop', 'the')])
+    result = aui.scan("the in of")
+    assert_equal(result, [('stop', 'the'),
+                          ('stop', 'in'),
+                          ('stop', 'of')])
+
+
+def test_nouns():
+    assert_equal(aui.scan("bear"), [('noun', 'bear')])
+    result = aui.scan("bear princess")
+    assert_equal(result, [('noun', 'bear'),
+                          ('noun', 'princess')])
+
+
+def test_numbers():
+    assert_equal(aui.scan("1234"), [('number', 1234)])
+    result = aui.scan("3 91234")
+    assert_equal(result, [('number', 3),
+                          ('number', 91234)])
+
+
+def test_errors():
+    assert_equal(aui.scan("ASDFADFASDF"),
+                 [('error', 'ASDFADFASDF')])
+    result = aui.scan("bear IAS princess")
+    assert_equal(result, [('noun', 'bear'),
+                          ('error', 'IAS'),
+                          ('noun', 'princess')])
