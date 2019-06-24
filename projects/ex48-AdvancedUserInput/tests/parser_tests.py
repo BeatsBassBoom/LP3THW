326752837b14e15d3e8289752a3eb7d67fabf706
28370491+BeatsBassBoom@users.noreply.github.com <28370491+BeatsBassBoom@users.noreply.github.com>
2019-06-23 20:28:25 -0400

    Skip test complete

--- projects/ex48-AdvancedUserInput/tests/parser_tests.py
+++ projects/ex48-AdvancedUserInput/tests/parser_tests.py
@@ -11,8 +11,10 @@ def test_ParserError():
 def test_Sentence():
     pass
 
-# analyzing the peek function with the word eat. 
+# analyzing the peek function with the word eat.
 # not sure why this works yet though.
+
+
 def test_peek():
     word_list = scan('princess go to the door')
     result = peek(word_list)
@@ -32,7 +34,7 @@ def test_match():
     second_expected_value = ('verb', 'go')
     third_expected_value = None
     fourth_expected_value = ('stop', 'to')
-    
+
     assert_equal(first_result, first_expected_value)
     assert_equal(second_result, second_expected_value)
     assert_equal(third_result, third_expected_value)
@@ -40,7 +42,14 @@ def test_match():
 
 
 def test_skip():
-    pass
+    word_list = scan('princess go to the door')
+
+    skip(word_list, 'noun')
+    skip(word_list, 'verb')
+    result = word_list
+    word_list.pop(0)
+    expected_value = word_list
+    assert_equal(result, expected_value)
 
 
 def test_parse_verb():
