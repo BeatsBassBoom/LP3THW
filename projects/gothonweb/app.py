e8313070d5b3f6544216b5be5af64fc38c521f7f
chaos <28370491+BeatsBassBoom@users.noreply.github.com>
2019-06-28 20:59:10 -0400

    HTML complete

--- projects/gothonweb/app.py
+++ projects/gothonweb/app.py
@@ -3,12 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def index():
     greeting = "Hello World"
-    return render_template("index.html", greeting=greeting)
-
+    return render_template('index.html', greeting=greeting)
 
 if __name__ == "__main__":
     app.run()
